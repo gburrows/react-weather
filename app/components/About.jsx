92eec4a113c23742ff42392f5084359ade727088
Graham Burrows <g.burrows85@gmail.com>
2017-11-12 19:05:24 +0000

    Changed copy on about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,14 +10,17 @@ var React = require('react');
 
 // turn into a stateless functional component (only for render functions in this case)
 
-//var About = (props) => {
-//	return (
-//		<h3>About Component</h3>
-//	)
-//};
+var About = (props) => {
+	return (
+		<div>
+			<h3>About</h3>
+			<p>Welcome to the about page!</p>
+		</div>
+	)
+};
 
 // or simpler by removing return etc and not needing "props" due to not using anything from props
 
-var About = () => <h3>About Component</h3>;
+//var About = () => <h3>About Component</h3>;
 
 module.exports = About;
